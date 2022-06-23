503a709243e3920a9304f171b403930612d9103b
rodolfo.djn@gmail.com <rodolfo.djn@gmail.com>
2022-06-23 15:59:59 -0300

    //COMPONENTE DE BOTÃO

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+
+import speedSvg from '../../assets/SpeedSvg.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
 
 
-import { Container,Header, CarImages } from './styles';
+import { Container,Header, CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Prince,
+    About,
+    Accessories
+
+} from './styles';
 
 export function CarDetails(){
     return (
@@ -19,6 +39,45 @@ export function CarDetails(){
 
             />
         </CarImages>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand> Lamborghini</Brand>
+                        <Name>hutacan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao Dia</Period>
+                        <Prince>R$ 580</Prince>
+                    </Rent>
+                </Details>
+
+                <Accessories>
+
+                   <Accessory name='380km/h' icon={speedSvg}/>
+                   <Accessory name='3.2s' icon={accelerationSvg}/>
+                   <Accessory name='800 HP' icon={forceSvg}/>
+                   <Accessory name='Gasolina' icon={gasolineSvg}/>
+                   <Accessory name='Auto' icon={exchangeSvg}/>
+                   <Accessory name='2 Pessoas' icon={peopleSvg}/>
+
+
+                </Accessories>
+
+                <About>
+                    Este é o automóvel desportivo. Surgiu do lendário tourode lide
+                    indultado na praça Real Maestranza de Sevilla. É um belissimo carro
+                    para quem gosta de acelerar.
+
+                </About>
+
+
+
+            </Content>
+
+
+
 
         </Container>
     );
