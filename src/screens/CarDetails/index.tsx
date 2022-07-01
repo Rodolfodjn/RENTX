f836a03e157e737001728271f2a9216e4c36ece6
rodolfo.djn@gmail.com <rodolfo.djn@gmail.com>
2022-07-01 15:23:58 -0300

    //correção

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,12 +3,12 @@ import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
 
-import speedSvg from '../../assets/SpeedSvg.svg';
-import accelerationSvg from '../../assets/acceleration.svg';
-import forceSvg from '../../assets/force.svg';
-import gasolineSvg from '../../assets/gasoline.svg';
-import exchangeSvg from '../../assets/exchange.svg';
-import peopleSvg from '../../assets/people.svg';
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
 
 
 import { Container,Header, CarImages,
@@ -71,12 +71,12 @@ export function CarDetails(){
 
                 <Accessories>
 
-                   <Accessory name='380km/h' icon={speedSvg}/>
-                   <Accessory name='3.2s' icon={accelerationSvg}/>
-                   <Accessory name='800 HP' icon={forceSvg}/>
-                   <Accessory name='Gasolina' icon={gasolineSvg}/>
-                   <Accessory name='Auto' icon={exchangeSvg}/>
-                   <Accessory name='2 Pessoas' icon={peopleSvg}/>
+                   <Accessory name='380km/h' icon={SpeedSvg}/>
+                   <Accessory name='3.2s' icon={AccelerationSvg}/>
+                   <Accessory name='800 HP' icon={ForceSvg}/>
+                   <Accessory name='Gasolina' icon={GasolineSvg}/>
+                   <Accessory name='Auto' icon={ExchangeSvg}/>
+                   <Accessory name='2 Pessoas' icon={PeopleSvg}/>
 
 
                 </Accessories>
