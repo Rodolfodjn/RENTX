aee86debbcd75eb923aaab206243cae435be5804
rodolfo.djn@gmail.com <rodolfo.djn@gmail.com>
2022-07-25 13:19:46 -0300

    funciounou

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRef } from 'react';
+import { FlatList, ViewToken } from 'react-native';
+import { CarDTO } from '../../dtos/CarDTO';
+import { Bullet } from '../Bullet';
 
 import {
     Container, 
@@ -7,30 +11,84 @@ import {
     CarImageWrapper, 
     CarImage } from './styles';
 
-    interface Props{
-        imageUrl: string[];
-    }
+    // interface Props {
+    //     imageUrl: {
+    //         id: string;
+    //         photo: string;
+    //     }[];
+    // };
 
-    export function ImageSlider({imageUrl}: Props) {
+interface Props {
+  imageUrl: string[];
+}
+
+
+// // 
+//     interface Props {
+//         id: string;
+//         photos: CarDTO["photos"];
+//       }
+
+
+    interface ChangeImageProps {
+        viewableItems: ViewToken[];
+        changed: ViewToken[];
+      }
+
+
+
+export function ImageSlider({ imageUrl }: Props) {
+
+        const [imageIndex, setImageIndex ] = useState(0);
+
+        const indexChanged = useRef((info: ChangeImageProps) => {
+            const index = info.viewableItems[0].index!;
+            setImageIndex(index);
+        });
+
+        // const indexChanged = useRef((info: ChangeImageProps) => {
+        //     setImageIndex(info.viewableItems[0].index!);
+        //   });
+
     return (
 
         <Container>
 
         <ImageIndexs>
+        {
+            // imageUrl.map((item, index)=> (
+            //     <Bullet key={String(item)}
+            //                 active={} 
+            //                 />
+            // ))
 
-        <ImageIndex active = {true} />
-        <ImageIndex active = {true} />
-        <ImageIndex active = {true} />
-        <ImageIndex active = {true} />
-        
+            imageUrl.map((_, index) => (
+                <ImageIndex 
+                key={String(index)} 
+                active={index === imageIndex} 
+                />
+              ))
+        }
+
         </ImageIndexs>
 
-        <CarImageWrapper>
-            <CarImage source = {{uri: ''}}
-            resizeMode= "contain"/>
-        
-        </CarImageWrapper>
+       
 
-        </Container>
-    );
+
+{/* <CarImageWrapper> */}
+        <FlatList
+          data={imageUrl}
+          keyExtractor={key => key}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          onViewableItemsChanged={indexChanged.current}
+          renderItem={({ item }) => (
+            <CarImageWrapper>
+              <CarImage source={{ uri: item }} resizeMode="contain" />
+            </CarImageWrapper>
+          )}
+        />
+      {/* </CarImageWrapper> */}
+    </Container>
+  );
 }
